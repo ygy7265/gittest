e8847916ccda4d1c951150e9b49aac0040454ebc
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2019-03-07 11:43:39 +0900

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by D7703_25 on 2019. 3. 7..
+//  Copyright © 2019년 D7703_25. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
